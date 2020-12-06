2e7d09c3eca48e08c6a0f6a37316e9331e36d89d
vivimouret29 <vivien.mouret@ynov.com>
2020-12-06 23:24:08 +0100

    ✨ display of the image taken by user

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import AppContextProvider from './data/AppContextProvider';
 import GameContextProvider from './data/GameContextProvider';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 ReactDOM.render(
 	<AppContextProvider>
@@ -17,3 +18,6 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Call the element loader after the app has been rendered the first time
+defineCustomElements(window);
