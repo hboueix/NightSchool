47467feb6541d8d8281524e576cf986c34060e5c
njkf <jules.vendee@ynov.com>
2020-12-06 20:15:18 +0100

    settings_dark-mode OK

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,12 @@
-import { IonBackButton, IonButtons, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButtons, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
+import { moon } from "ionicons/icons";
 import React from 'react';
 
 const Settings: React.FC = () => {
-
+	const toggleDarkModeHandler = () => {
+		document.body.classList.toggle("dark");
+	  };
+	  
 	return (
 		<IonPage>
 			<IonHeader>
@@ -13,6 +17,17 @@ const Settings: React.FC = () => {
 					<IonTitle>Paramètres</IonTitle>
 				</IonToolbar>
 			</IonHeader>
+			<IonContent>
+				<IonItem>
+				<IonIcon slot="start" icon={moon} />
+				<IonLabel>Dark Mode</IonLabel>
+				<IonToggle
+				slot="end"
+				name="darkMode"
+				onIonChange={toggleDarkModeHandler}
+				/>
+			</IonItem>
+			</IonContent>
 		</IonPage>
 	)
 }
